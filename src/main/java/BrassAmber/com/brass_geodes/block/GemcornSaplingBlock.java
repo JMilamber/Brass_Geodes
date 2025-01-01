d32a73849aa51e38d15266eb677398c4a3a7d40c
Milamber <socky79.turbulance@gmail.com>
2025-01-01 13:35:52 -0500

    fix gemcorn blocks

--- src/main/java/BrassAmber/com/brass_geodes/block/GemcornSaplingBlock.java
+++ src/main/java/BrassAmber/com/brass_geodes/block/GemcornSaplingBlock.java
@@ -1,11 +1,58 @@
 package BrassAmber.com.brass_geodes.block;
 
+import net.minecraft.core.BlockPos;
+import net.minecraft.server.level.ServerLevel;
+import net.minecraft.util.RandomSource;
+import net.minecraft.world.level.BlockGetter;
+import net.minecraft.world.level.Level;
+import net.minecraft.world.level.LevelReader;
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.level.block.SaplingBlock;
 import net.minecraft.world.level.block.grower.AbstractTreeGrower;
+import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.level.block.state.StateDefinition;
+import net.minecraft.world.level.block.state.properties.BlockStateProperties;
+import net.minecraft.world.level.block.state.properties.IntegerProperty;
 
 public class GemcornSaplingBlock extends SaplingBlock {
+    public static final IntegerProperty AGE = BlockStateProperties.AGE_3;
+    public static final int MAX_AGE = 3;
 
     public GemcornSaplingBlock(AbstractTreeGrower treeGrower, Properties properties) {
         super(treeGrower, properties);
+        this.registerDefaultState(this.stateDefinition.any().setValue(STAGE, 0).setValue(AGE, 0));
     }
+
+    protected void createBlockStateDefinition(StateDefinition.Builder<Block, BlockState> p_221484_) {
+        p_221484_.add(STAGE).add(AGE);
+    }
+
+    protected boolean mayPlaceOn(BlockState blockState, BlockGetter blockGetter, BlockPos blockPos) {
+        return blockState.is(Blocks.STONE);
+    }
+
+    public void randomTick(BlockState blockState, ServerLevel serverLevel, BlockPos blockPos, RandomSource randomSource) {
+        if (isFullyGrown(blockState)) {
+            if (randomSource.nextInt(14) == 0) {
+                this.advanceTree(serverLevel, blockPos, blockState, randomSource);
+            }
+        } else {
+            serverLevel.setBlock(blockPos, blockState.cycle(AGE), 2);
+        }
+
+    }
+
+    private static boolean isFullyGrown(BlockState p_221502_) {
+        return p_221502_.getValue(AGE) == 3;
+    }
+
+    public boolean isValidBonemealTarget(LevelReader levelReader, BlockPos blockPos, BlockState blockState, boolean bool) {
+        return !isFullyGrown(blockState);
+    }
+
+    public boolean isBonemealSuccess(Level level, RandomSource randomSource, BlockPos blockPos, BlockState blockState) {
+        return (double)level.random.nextFloat() < 0.1D;
+    }
+
 }
